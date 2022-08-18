0fa62839a449bb423ff7d2e7db60e2edf20c423b
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-18 23:08:00 +0530

    js programming construct problems

---
+++ Selection_if_else/DayOfMonthWithinRangePrintsTrue.js
@@ -0,0 +1,21 @@
+// prints true if day of month is between March 20 and June 20, false otherwise.
+let date = prompt("enter date (dd)");
+let month = prompt("enter month (mm)")
+
+if (month == 03) {
+    if(date >= 20){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+} else if(month == 04 || month == 5){
+    console.log(true);
+} else if( month == 6){
+    if(date <= 20){
+        sconsole.log(true);
+    }else{
+        console.log(false);
+    }
+}else{
+    console.log(false);
+}
---
+++ Selection_if_else/ReadNumberAndDisplayWeekDay.js
@@ -0,0 +1,4 @@
+let number = prompt("Enter Number betn 1 to 7 ");
+let numbers=[null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+console.log(numbers[number]);
